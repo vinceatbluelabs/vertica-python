be2790e061da93488f34a23e3e8f5ebf1d3de483
Vince Broz <vince.broz@bluelabs.com>
2018-11-21 12:11:56 -0500

    i

--- vertica_python/vertica/cursor.py
+++ vertica_python/vertica/cursor.py
@@ -214,13 +214,13 @@ class Cursor(object):
     def setoutputsize(self, size, column=None):
         pass
 
-    #############################################
-    # non-dbapi methods
-    #############################################
+    #
+    # Non dbApi methods
+    #
     def flush_to_query_ready(self):
         # if the last message isn't empty or ReadyForQuery, read all remaining messages
         if(self._message is None
-                or isinstance(self._message, messages.ReadyForQuery):
+                or isinstance(self._message, messages.ReadyForQuery)):
             return
 
         while True:
@@ -231,7 +231,7 @@ class Cursor(object):
                 break
 
     def flush_to_command_complete(self):
-        # if the last message isnt empty or CommandComplete, read messages until it is
+        # if the last message isn't empty or CommandComplete, read messages until it is
         if(self._message is None
            or isinstance(self._message, messages.ReadyForQuery)
            or isinstance(self._message, messages.CommandComplete)):
