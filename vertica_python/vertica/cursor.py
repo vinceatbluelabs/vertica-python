7b39da525b5c27bb6573ef9578a82cd63d958e4f
Vince Broz <vince.broz@bluelabs.com>
2018-11-21 12:19:31 -0500

    n

--- vertica_python/vertica/cursor.py
+++ vertica_python/vertica/cursor.py
@@ -124,7 +124,8 @@ class Cursor(object):
                 self.rowcount = 1
             else:
                 self.rowcount += 1
-             row = self.row_formatter(self._message)
+
+            row = self.row_formatter(self._message)
             # fetch next message
             self._message = self.connection.read_message()
             return row
